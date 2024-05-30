f2718118f19ce11c4ce698770012610605e3b1ab
Senghoung <Senghoung125@gmail.com>
2024-05-30 03:09:13 -0700

    Adding Vite deployment config and yml file

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //Deploy on GitHub
+  base: "/PrescriptionWebPage/" //Repo Name
 })
