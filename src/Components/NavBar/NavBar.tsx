96128bf06c401d0b45e551b6f4397745e1861a6f
Senghoung <Senghoung125@gmail.com>
2024-05-30 02:10:31 -0700

    Fixed Title Spacing and Name

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 import './NavBar.css';
 const NavBar = () => {
-  return <h2 className='prescription-web-title'> Prescription Upload</h2>;
+  return <h2 className='prescription-web-title'> Prescription QR Code</h2>;
 };
 
 export default NavBar;
