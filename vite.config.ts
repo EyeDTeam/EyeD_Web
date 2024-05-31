067a7b691bd6661717abad20c34288bb10b0828c
Senghoung <Senghoung125@gmail.com>
2024-05-30 20:19:49 -0700

    Using single quote for base in vite.config.ts to check if that causing the deployment error

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   //Deploy on GitHub
-  base: "/PrescriptionWebPage/" //Repo Name
+  base: '/PrescriptionWebPage/' //Repo Name
 })
